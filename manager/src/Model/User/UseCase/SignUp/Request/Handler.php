14faf61ac099cb36b06960f877b23e6ebca5c7a7
Aleksey Belchenkov <u608110@gmail.com>
2021-05-05 19:33:25 +0400

    Added user UUID id

--- manager/src/Model/User/UseCase/SignUp/Request/Handler.php
+++ manager/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace App\Model\User\UseCase\SignUp\Request;
 
-use App\Entity\User;
+use App\Model\User\Entity\User\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\Uuid;
 
 class Handler
 {
@@ -25,6 +26,7 @@ class Handler
         }
 
         $user = new User(
+            Uuid::uuid4()->toString(),
             $email,
             password_hash($command->password, PASSWORD_ARGON2I)
         );
--- manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
+++ manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
@@ -4,18 +4,21 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Model\User\Entity\User\SignUp;
 
-use App\Entity\User;
+use App\Model\User\Entity\User\User;
 use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
 
 class RequestTest extends TestCase
 {
     public function testSuccess(): void
     {
         $user = new User(
+            $id = Uuid::uuid4()->toString(),
             $email = 'test@app.test',
             $hash = 'hash'
         );
 
+        self::assertEquals($id, $user->getId());
         self::assertEquals($email, $user->getEmail());
         self::assertEquals($hash, $user->getPasswordHash());
     }
