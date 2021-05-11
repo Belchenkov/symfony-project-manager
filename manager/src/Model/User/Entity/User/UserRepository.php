4774793b1be72097060758da7509a1564ddd30bd
Aleksey Belchenkov <u608110@gmail.com>
2021-05-11 14:58:45 +0400

    Added role

--- manager/src/Model/User/Entity/User/UserRepository.php
+++ manager/src/Model/User/Entity/User/UserRepository.php
@@ -8,6 +8,8 @@ interface UserRepository
 {
     public function findByConfirmToken(string $token): ?User;
 
+    public function get(Id $id): User;
+
     public function hasByEmail(Email $email): bool;
 
     public function getByEmail(Email $email): User;
