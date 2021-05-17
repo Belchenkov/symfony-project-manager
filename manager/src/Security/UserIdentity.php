7a5612f88ba42ad341df45944de74ed3dc08a0a2
Aleksey Belchenkov <u608110@gmail.com>
2021-05-17 18:47:11 +0400

    Added auth active checking

--- manager/src/Security/UserIdentity.php
+++ manager/src/Security/UserIdentity.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Security;
 
+use App\Model\User\Entity\User\User;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserIdentity implements UserInterface
@@ -12,18 +13,21 @@ class UserIdentity implements UserInterface
     private $username;
     private $password;
     private $role;
+    private $status;
 
     public function __construct(
         string $id,
         string $username,
         string $password,
-        string $role
+        string $role,
+        string $status
     )
     {
         $this->id = $id;
         $this->username = $username;
         $this->password = $password;
         $this->role = $role;
+        $this->status = $status;
     }
 
     public function getId(): string
@@ -49,6 +53,11 @@ class UserIdentity implements UserInterface
     public function getSalt(): ?string
     {
         return null;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status === User::STATUS_ACTIVE;
     }
 
     public function eraseCredentials(): void
